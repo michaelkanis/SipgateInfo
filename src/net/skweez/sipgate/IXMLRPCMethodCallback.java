a005af98a7a263b6977c7cdf6e7f943a9c119676
Michael Kanis <mks@skweez.net>
2011-03-26 10:57:01 +0100

    Remove wrong header comments

--- src/net/skweez/sipgate/IXMLRPCMethodCallback.java
+++ src/net/skweez/sipgate/IXMLRPCMethodCallback.java
@@ -1,17 +1,3 @@
-/*-----------------------------------------------------------------------+
- | sipgate Kontostand
- |                                                                       |
-   $Id: codetemplates.xml 9 2009-03-06 10:09:52Z mks $            
- |                                                                       |
- | Copyright (c)  2004-2011 Technische Universitaet Muenchen             |
- |                                                                       |
- | Technische Universitaet Muenchen               #########  ##########  |
- | Institut fuer Informatik - Lehrstuhl IV           ##  ##  ##  ##  ##  |
- | Prof. Dr. Manfred Broy                            ##  ##  ##  ##  ##  |
- | Boltzmannstr. 3                                   ##  ##  ##  ##  ##  |
- | 85748 Garching bei Muenchen                       ##  ##  ##  ##  ##  |
- | Germany                                           ##  ######  ##  ##  |
- +-----------------------------------------------------------------------*/
 package net.skweez.sipgate;
 
 /**
