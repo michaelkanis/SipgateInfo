79919550ffaab40a6a0a7accfac5ae921e77f7b0
Michael Kanis <mks@skweez.net>
2011-03-27 17:52:27 +0200

    Integrate settings into account tab

--- src/net/skweez/sipgate/SipgateTabActivity.java
+++ src/net/skweez/sipgate/SipgateTabActivity.java
@@ -1,9 +1,19 @@
 package net.skweez.sipgate;
 
+import java.net.Authenticator;
+import java.util.Observable;
+import java.util.Observer;
+
+import net.skweez.sipgate.model.Balance;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 /**
  * 
@@ -12,7 +22,10 @@ import android.widget.TabHost;
  * @version $Rev: 9 $
  * @levd.rating RED Rev:
  */
-public class SipgateTabActivity extends TabActivity {
+public class SipgateTabActivity extends TabActivity implements Observer {
+
+	/** The view that shows the account balance. */
+	private TextView balanceView;
 
 	/** {@inheritDoc} */
 	@Override
@@ -24,7 +37,7 @@ public class SipgateTabActivity extends TabActivity {
 		TabHost.TabSpec tabSpec;
 		Intent intent;
 
-		intent = new Intent().setClass(this, AccountActivity.class);
+		intent = new Intent().setClass(this, SetupActivity.class);
 		tabSpec = tabHost.newTabSpec("account").setIndicator("Account")
 				.setContent(intent);
 		tabHost.addTab(tabSpec);
@@ -33,5 +46,53 @@ public class SipgateTabActivity extends TabActivity {
 		tabSpec = tabHost.newTabSpec("calls").setIndicator("Calls")
 				.setContent(intent);
 		tabHost.addTab(tabSpec);
+
+		Authenticator.setDefault(new PreferencesAuthenticator(PreferenceManager
+				.getDefaultSharedPreferences(this)));
+
+		balanceView = (TextView) findViewById(R.id.balanceView);
+		refreshBalance();
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.refresh:
+			refreshBalance();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/** Tells the balance object to refresh itself. */
+	private void refreshBalance() {
+		balanceView.setText("Loading …");
+
+		Balance balance = new Balance();
+		balance.addObserver(this);
+		balance.startRefresh();
+	}
+
+	/** Update the view when the balance object updated itself. */
+	public void update(final Observable observable, final Object data) {
+		if (observable instanceof Balance) {
+			balanceView.post(new Runnable() {
+				public void run() {
+					balanceView.setText(((Balance) observable).getBalance()
+							.toString());
+				}
+			});
+		}
 	}
 }
