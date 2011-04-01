e06aab31184cd84ab720b35dd137155a41aba702
Florian Mutter <elm@skweez.net>
2011-04-01 23:55:29 +0200

    inital layout for the account info tab

--- src/net/skweez/sipgate/AccountInfoActivity.java
+++ src/net/skweez/sipgate/AccountInfoActivity.java
@@ -5,7 +5,6 @@ import java.util.Observer;
 
 import net.skweez.sipgate.model.UserInfos;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,13 +12,14 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-public class AccountInfoActivity extends ListActivity {
+public class AccountInfoActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setListAdapter(new UserInfoAdapter(this));
+		// setListAdapter(new UserInfoAdapter(this));
+		setContentView(R.layout.accountinfo);
 
 	}
 
