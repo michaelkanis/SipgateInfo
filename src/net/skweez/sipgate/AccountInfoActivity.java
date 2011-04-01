d2896d4ee940c6f5e384984fbf0ab3f15755a8dc
Florian Mutter <elm@skweez.net>
2011-04-01 15:16:25 +0200

    UserInfoAdapter is now a private class in AccountInfoActivity

--- src/net/skweez/sipgate/AccountInfoActivity.java
+++ src/net/skweez/sipgate/AccountInfoActivity.java
@@ -1,7 +1,17 @@
 package net.skweez.sipgate;
 
+import java.util.Observable;
+import java.util.Observer;
+
+import net.skweez.sipgate.model.UserInfos;
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class AccountInfoActivity extends ListActivity {
 
@@ -11,5 +21,56 @@ public class AccountInfoActivity extends ListActivity {
 
 		setListAdapter(new UserInfoAdapter(this, this));
 
+	}
+
+	private class UserInfoAdapter extends BaseAdapter implements Observer {
+
+		private UserInfos userInfos;
+		private Context mContext;
+		private Activity mActivity;
+
+		public UserInfoAdapter(Context context, Activity activity) {
+			this.mContext = context;
+			this.mActivity = activity;
+
+			userInfos = new UserInfos();
+			userInfos.addObserver(this);
+			userInfos.startRefresh();
+		}
+
+		public int getCount() {
+			return userInfos.length;
+		}
+
+		public Object getItem(int position) {
+			return position;
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView tv;
+			if (convertView == null) {
+				tv = new TextView(this.mContext);
+			} else {
+				tv = (TextView) convertView;
+			}
+
+			tv.setText(userInfos.getOwnURIs()[position].e164Out);
+
+			return tv;
+		}
+
+		public void update(Observable observable, Object data) {
+			if (observable instanceof UserInfos) {
+				mActivity.runOnUiThread(new Runnable() {
+					public void run() {
+						notifyDataSetChanged();
+					}
+				});
+			}
+		}
 	}
 }
