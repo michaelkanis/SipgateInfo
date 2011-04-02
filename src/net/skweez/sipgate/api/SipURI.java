df7e1455de12b987cd58bf14566b99cf7af048d7
Michael Kanis <mkanis@gmx.de>
2011-04-02 12:18:25 +0200

    Parse SIP URI and show only the number in toString

--- src/net/skweez/sipgate/api/SipURI.java
+++ src/net/skweez/sipgate/api/SipURI.java
@@ -2,19 +2,50 @@ package net.skweez.sipgate.api;
 
 public class SipURI {
 
-	private final String uri;
+	private final char PROTOCOL_SEPARATOR = ':';
+
+	private final char DOMAIN_SEPARATOR = '@';
+
+	private final String protocol;
+
+	private final String number;
+
+	private final String domain;
 
 	public SipURI(String uri) {
-		this.uri = uri;
+		int start = 0;
+		int end = uri.indexOf(PROTOCOL_SEPARATOR);
+		this.protocol = uri.substring(start, end);
+
+		start = end + 1;
+		end = uri.indexOf(DOMAIN_SEPARATOR);
+		this.number = uri.substring(start, end);
+
+		start = end + 1;
+		this.domain = uri.substring(start);
 	}
 
 	public String getUri() {
-		return uri;
+		return new StringBuilder(protocol).append(PROTOCOL_SEPARATOR)
+				.append(number).append(DOMAIN_SEPARATOR).append(domain)
+				.toString();
 	}
-	
+
+	public String getProtocol() {
+		return protocol;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
 	@Override
 	public String toString() {
-		return getUri();
+		return getNumber();
 	}
 
 }
