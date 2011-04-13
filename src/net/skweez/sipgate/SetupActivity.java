fbd698b36e88a280810cbe73649472d2578568e9
Michael Kanis <mkanis@gmx.de>
2011-04-13 21:10:43 +0200

    Add version information to settings activity

--- src/net/skweez/sipgate/SetupActivity.java
+++ src/net/skweez/sipgate/SetupActivity.java
@@ -2,25 +2,43 @@ package net.skweez.sipgate;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 public class SetupActivity extends PreferenceActivity implements
 		OnSharedPreferenceChangeListener {
+
+	private static final String TAG = SetupActivity.class.getName();
 
 	private EditTextPreference userNamePreference;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.setup);
-		
+
 		addPreferencesFromResource(R.xml.preferences);
 
 		userNamePreference = (EditTextPreference) getPreferenceScreen()
 				.findPreference("username");
+
+		// Get the version number and set it as the summary of the version pref.
+		PackageManager pm = getPackageManager();
+		PackageInfo info = null;
+		try {
+			info = pm.getPackageInfo("net.skweez.sipgate", 0);
+		} catch (NameNotFoundException e) {
+			Log.e(TAG, "Cannot find package info.");
+		}
+		Preference version = findPreference(getString(R.string.version_key));
+		version.setSummary(info.versionName);
 	}
 
 	/** {@inheritDoc} */
